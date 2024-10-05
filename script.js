d8943b8510593c1ab6425e817aea1e4bdf71d4f9
SaritahXhi <saritakurt87@gmail.com>
2024-10-04 20:53:18 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-document.querySelector('.menu-toggle').addEventListener('click', function () {
-    const menu = document.querySelector('.menu');
-    menu.classList.toggle('show');
-});
+<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        const menuToggle = document.querySelector('.menu-toggle');
+        const menu = document.querySelector('.menu');
+
+        menuToggle.addEventListener('click', function() {
+            menu.classList.toggle('active');
+        });
+    });
+</script>
